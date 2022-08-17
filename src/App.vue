141a57cc08bea93c3eac82db6029f10c839ed25a
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-08-17 13:51:21 +0700

    Back end Refactoring

--- src/App.vue
+++ src/App.vue
@@ -27,42 +27,69 @@ export default {
     }
   },
   methods: {
-    addTask(task) {
-      this.tasks = [...this.tasks, task]
+    async addTask(task) {
+      const res = await fetch('api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task)
+      })
+
+      const data = await res.json()
+
+      this.tasks = [...this.tasks, data]
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       if(confirm('Apakah anda yakin?')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id)
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE'
+        })
+
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Ada Error Saat Hapus Task')
+
       }
     },
-    toggleReminder(id) {
-      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    async toggleReminder(id) {
+
+      // Fetching specific data
+      const taskToToggle = await this.fetchTask(id)
+      const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+      const res =  await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(updTask)
+      })
+
+      const data = await res.json()
+
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: data.reminder } : task)
     },
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
-    }
+    },
+    async fetchTasks() {
+      const res = await fetch('api/tasks')
+
+      const data = await res.json()
+
+
+      return data
+    },
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`)
+
+      const data = await res.json()
+
+      return data
+    },
+
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Dokter Main',
-        day: '1 Maret jam 2.30',
-        reminder: true
-      },
-      {
-        id: 2,
-        text: 'Ke sekolah',
-        day: '3 Maret jam 01.30',
-        reminder: true
-      },
-      {
-        id: 3,
-        text: 'Belanja Makanan',
-        day: '3 Maret jam 11:00',
-        reminder: false
-      },
-    ]
+  async created() {
+    this.tasks = await this.fetchTasks()
   }
 }
 </script>
