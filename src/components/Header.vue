647af7f4b8d2219176194eda1bc188a5fcf817a1
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2022-08-17 09:57:01 +0700

    Add Styling

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button @click="onClick()" text="Tambah Task" color="green" />
+  </header>
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+    name: 'Header',
+    components: {
+        Button
+    },
+    props: {
+        title: String,
+    },
+    methods:{
+        onClick(){
+            console.log('klik')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
